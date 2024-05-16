3034675cd371649c3df490529ea61ad61954de1c
Sofia Sivilotti <68237440+ssivilotti@users.noreply.github.com>
2024-05-16 12:21:19 -0500

    implement surface plotting

--- chemical_space.py
+++ chemical_space.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import itertools
+import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 class ChemicalSpace:
@@ -11,28 +12,20 @@ class ChemicalSpace:
         self.shape = self.yield_surface.shape
         self.all_points = list(itertools.product(*[range(s) for s in self.shape]))
         self._y_true = np.array([self.yield_surface[point] for point in self.all_points])
+        self.all_condtions = list(itertools.product(*[range(s) for s in self.shape[:self.condtions_dim]]))
 
-    def create_mat(self, data_file, cond_titles:list, reactant_titles:list,  target_title)->np.ndarray:
+    def _create_mat(self, data_file, cond_titles:list, reactant_titles:list,  target_title)->np.ndarray:
         # Create a matrix of the data
         data = pd.read_csv(data_file)
         unique_rs = [data[r_title].unique() for r_title in reactant_titles]
         unique_conds = [data[cond_title].unique() for cond_title in cond_titles]
-        titles = cond_titles + reactant_titles
+        self.titles = cond_titles + reactant_titles
         self.labels = unique_conds + unique_rs
         shape = [len(item) for item in self.labels]
         data_mat = np.zeros(tuple(shape))
-        # for idx in itertools.product(*[range(s) for s in shape]):
-            # data_mat[idx] = data[(data[reactant_titles[0]] == unique_rs[0][idx[0]]) & (data[reactant_titles[1]] == unique_rs[1][idx[1]]) & (data[cond_titles[0]] == unique_conds[0][idx[2])][target_title].values[0]
         for idx, series in data.iterrows():
-            point = [np.where(self.labels[i] == (series[title])) for i, title in enumerate(titles)]
+            point = [np.where(self.labels[i] == (series[title])) for i, title in enumerate(self.titles)]
             data_mat[tuple(point)] = series[target_title]
-        # for i, r1 in enumerate(unique_r1):
-        #     for j, r2 in enumerate(unique_r2):
-        #         for k, cond in enumerate(unique_cond):
-        #             data_mat[k, i, j] = data[(data[r1_title] == r1) & (data[r2_title] == r2) & (data[cond_title] == cond)][target_title].values[0]
-        # for i in range(len(unique_r1)):
-        #     for j in range(len(unique_r2)):
-        #         data_mat[i, j] = data[(data[r1_title] == r1[i]) & (data[r2_title] == r2[j])]['yield'].values[0]
         return data_mat
 
     def measure_reaction_yield(self, point:list) -> float:
@@ -42,5 +35,65 @@ class ChemicalSpace:
         y_pred = y_pred.T[1] > .5
         y_true = self._y_true > cutoff
         return accuracy_score(y_true, y_pred), precision_score(y_true, y_pred), recall_score(y_true, y_pred)
+    
+    def _get_condition_surface(self, condition:tuple) -> np.ndarray:
+        s = self.yield_surface[condition[0]]
+        for c in condition[1:]:
+            s = s[c]
+        return s
+    
+    def count_coverage(self, condtion_set:tuple, yield_threshold:float) -> int:
+        yield_coverage_surface = np.zeros(self.shape[self.condtions_dim:])
+        for cond in condtion_set:
+            s = self._get_condition_surface(cond)
+            yield_coverage_surface = np.maximum(yield_coverage_surface, s)
+        return np.sum(yield_coverage_surface > yield_threshold)
 
+    def best_condition_sets(self, yield_threshold:float, max_set_size:int=1, num_sets:int=10) -> tuple:
+        possible_combos = list(itertools.combinations(self.all_condtions, max_set_size))
+        coverages = [self.count_coverage(set, yield_threshold) for set in possible_combos]
+        if max_set_size > 1:
+            best_condition_sets_smaller, best_coverages_smaller = self.best_condition_sets(yield_threshold, max_set_size-1, num_sets)
+            possible_combos = possible_combos + best_condition_sets_smaller
+            coverages = coverages + best_coverages_smaller
+        best_set_idxs = np.argsort(coverages)[-num_sets:]
+        best_sets = [possible_combos[i] for i in best_set_idxs]
+        best_coverages = [coverages[i] for i in best_set_idxs]
+        return best_sets, best_coverages
+    
+    def format_condition(self, condition:tuple) -> str:
+        return ', '.join([f'{self.labels[i][c]}' for i, c in enumerate(condition)])
 
+    def plot_conditions(self, file_name, conditions:tuple, yield_threshold=0) -> None:
+        '''
+        conditions: tuple of condtion tuples
+        '''
+        ylabel = self.titles[self.condtions_dim]
+        xlabel = self.titles[self.condtions_dim + 1]
+        yields = np.zeros(self.shape[self.condtions_dim:])
+        for condition in conditions:
+            yields = np.maximum(yields, self._get_condition_surface(condition))
+        below_threshold = yields < yield_threshold
+        yields[below_threshold] = 0
+        plt.imshow(yields, vmin=0, vmax=100)
+        plt.colorbar(label='% Yield')
+        plt.xlabel(xlabel, fontsize=18)
+        plt.ylabel(ylabel, fontsize=18)
+        condition_str = '; '.join([self.format_condition(c) for c in conditions])
+        if len(conditions) == 1:
+            condition_str = f"Condition {conditions[0]} Yield"
+        else:
+            condition_str = f"Conditions {condition_str} Max Yield"
+        plt.title(f'{condition_str}', fontsize=20)
+        plt.savefig(file_name)
+
+    def plot_surface(self, dataset_name, zlabel = '% Yield', title='Highest Yield Across All Condtions') -> None:    
+        ylabel = self.titles[self.condtions_dim]
+        xlabel = self.titles[self.condtions_dim + 1]
+        max_yield_surface = np.amax(self.yield_surface.T, axis=-1*(self.condtions_dim)).T
+        plt.imshow(max_yield_surface)
+        plt.colorbar(label=zlabel)
+        plt.xlabel(xlabel, fontsize=15)
+        plt.ylabel(ylabel, fontsize=15)
+        plt.title(title, fontsize=20)
+        plt.savefig(f'{dataset_name}.png')

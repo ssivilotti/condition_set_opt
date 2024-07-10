f1410e783a6812519a0389f7e11340578d1e6bec
Sofia Sivilotti <68237440+ssivilotti@users.noreply.github.com>
2024-07-10 12:08:19 -0500

    regressor random selection

--- learners/random_selection.py
+++ learners/random_selection.py
@@ -1,20 +1,16 @@
 import numpy as np
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from space_mat import SpaceMatrix
-from learners.learner import Classifier
+from learners.learner import Classifier, YieldPred
 
 class ALClassifierRandomSelection(Classifier):
     '''Randomly selects the next points to be measured, benchmarking against active learning classifiers'''
-    def __init__(self, space_shape:int):
-        super().__init__(space_shape)
+    def __init__(self, space_shape:int, cpus=None):
+        super().__init__(space_shape, cpus)
 
-    def suggest_next_n_points(self, X:np.ndarray, n:int, measured_indices:np.ndarray)->list:
+    def suggest_next_n_points(self, X:np.ndarray, n:int, measured_indices:set)->list:
         '''next_points is a list of indices of the next points to be measured'''
         uncertainty = self.predict(X)
 
-        if len(uncertainty) == 0:
-            return []
+        assert len(uncertainty) != 0, 'Failed to predict uncertainty'
 
         num_options = np.prod(self.shape)
 
@@ -23,8 +19,27 @@ class ALClassifierRandomSelection(Classifier):
         next_points = np.random.choice(options, n, replace=False).tolist()
         
         point_uncertainties = (abs(np.array([uncertainty[i][0] for i in next_points]) - .5) / .5).tolist()
-        avg_point_certainty = np.average(point_uncertainties)
 
-        # self.done = avg_point_certainty > .5
-        self.done = False
         return uncertainty, self.predicted_surface, next_points, point_uncertainties
+    
+class ALYieldPredRandomSelection(YieldPred):
+    '''Randomly selects the next points to be measured, benchmarking against active learning classifiers'''
+    def __init__(self, space_shape:int):
+        super().__init__(space_shape)
+
+    def suggest_next_n_points(self, X:np.ndarray, n:int, measured_indices:set)->list:
+        '''next_points is a list of indices of the next points to be measured'''
+        yield_std = self.predict(X)
+
+        if len(yield_std) == 0:
+            return []
+
+        num_options = np.prod(self.shape)
+
+        options = np.array([i for i in range(num_options) if not (i in measured_indices)])
+
+        next_points = np.random.choice(options, n, replace=False).tolist()
+        
+        point_uncertainties = np.array([yield_std[i] for i in next_points])
+
+        return yield_std, self.predicted_surface, next_points, point_uncertainties

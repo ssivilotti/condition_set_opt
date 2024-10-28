1fa1c8d5cab647aa0bef8a45770e738f57c71a09
Sofia Sivilotti <68237440+ssivilotti@users.noreply.github.com>
2024-10-28 16:29:24 -0500

    fix imports

--- learners/random_selection.py
+++ learners/random_selection.py
@@ -1,5 +1,5 @@
 import numpy as np
-from learners.learner import Classifier, YieldPred
+from learners.learner import Classifier
 
 class ALClassifierRandomSelection(Classifier):
     '''Randomly selects the next points to be measured, benchmarking against active learning classifiers'''
@@ -20,26 +20,5 @@ class ALClassifierRandomSelection(Classifier):
         
         point_uncertainties = (abs(np.array([uncertainty[i][0] for i in next_points]) - .5) / .5).tolist()
 
+
         return uncertainty, self.predicted_surface, next_points, point_uncertainties
-    
-class ALYieldPredRandomSelection(YieldPred):
-    '''Randomly selects the next points to be measured, benchmarking against active learning classifiers'''
-    def __init__(self, space_shape:int):
-        super().__init__(space_shape)
-
-    def suggest_next_n_points(self, X:np.ndarray, n:int, measured_indices:set)->list:
-        '''next_points is a list of indices of the next points to be measured'''
-        yield_std = self.predict(X)
-
-        if len(yield_std) == 0:
-            return []
-
-        num_options = np.prod(self.shape)
-
-        options = np.array([i for i in range(num_options) if not (i in measured_indices)])
-
-        next_points = np.random.choice(options, n, replace=False).tolist()
-        
-        point_uncertainties = np.array([yield_std[i] for i in next_points])
-
-        return yield_std, self.predicted_surface, next_points, point_uncertainties
